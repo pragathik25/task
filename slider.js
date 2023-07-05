e0c34b80efcb6b09b2a9ea27da042b4a711287d8
pragathik25 <pkshetty4446@gmail.com>
2023-07-05 18:08:40 +0530

    slider

---
+++ slider.js
@@ -0,0 +1,26 @@
+$(function() {
+    $('.slider__wrapper').slick({
+        fade: true,
+    });
+    $('.slider__wrapper1').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 5
+    });
+    $('.slider__wrapper3').slick({
+        fade: true,
+    });
+
+});
+
+// $(function() {
+//     $('.slider__wrapper1').slick({
+//         infinite: true,
+//         slidesToShow: 5,
+//         slidesToScroll: 5
+//     });
+// });
+
+// $(function() {
+
+// });
